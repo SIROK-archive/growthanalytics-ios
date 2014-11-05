7a0526c805dc04c1de4a029194e3e594e92514e3
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-11-06 08:00:57 +0900

    Add empty Cocoa Touch Framework

---
+++ source/GrowthAnalytics/GrowthAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  GrowthAnalytics.h
+//  GrowthAnalytics
+//
+//  Created by Kataoka Naoyuki on 2014/11/06.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GrowthAnalytics.
+FOUNDATION_EXPORT double GrowthAnalyticsVersionNumber;
+
+//! Project version string for GrowthAnalytics.
+FOUNDATION_EXPORT const unsigned char GrowthAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GrowthAnalytics/PublicHeader.h>
+
+
