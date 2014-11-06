f430fc1c6553c4be9fb4ecf152a6288842092bb6
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-11-06 14:44:48 +0900

    Create sample project

---
+++ sample/GrowthAnalyticsSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrowthAnalyticsSample
+//
+//  Created by Kataoka Naoyuki on 2014/11/06.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/GrowthAnalyticsSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GrowthAnalyticsSample
+//
+//  Created by Kataoka Naoyuki on 2014/11/06.
+//  Copyright (c) 2014年 SIROK, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
